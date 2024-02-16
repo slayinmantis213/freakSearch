c185be79f80649a81ed3b0c47d0dbdaa9001ae21
DustinYansberg <DustinYansberg@gmail.com>
2024-02-16 10:15:39 -0800

    added data annotations to reflect table name and episode column name

--- Models/Episode.cs
+++ Models/Episode.cs
@@ -4,14 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace freakSearch.Models;
 
+[Table("freakonomics_episodes")]
 public class Episode
 {
     [Key]
-    public int EpisodeId { get; set; }
-
-    [Required]
+    public int Id { get; set; }
     public string Title { get; set; }
-
+    [Column("NUMBER")]
     public string EpisodeNumber { get; set; }
     public string Summary { get; set; }
     public string Link { get; set; }
